603095dc38c83fa801463b571850d8dd78521100
gaurav9969351313 <gauravtalele@gmail.com>
2019-10-19 20:25:10 +0530

    first commit

---
+++ Hello World/spec/hello-testing.spec.js
@@ -0,0 +1,8 @@
+var Hello = require("../hello-testing");
+
+describe("Hello module", function() {
+  it("should return a message", function() {
+   // expect(Hello.testing()).toEqual('Hello, testing!');
+   expect(Hello.testing()).not.toEqual('Hello, testing!');
+  });
+});
